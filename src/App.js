06ee9561074ae1e15ef0528fb42ca104d3732a79
Vishnupriya.S <pradeep.d@mu-sigma.com>
2024-03-23 14:37:10 +0530

    react project one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import { useState } from 'react';
+import SearchItem from "./SearchItem";
+
+
+
 
 function App() {
+
+
+  const [items, setItems] = useState([{
+    id:1,
+    checked :true,
+    item : 'Learn'
+
+  }]);  
+
+const [newItem,setNewItem] = useState('')
+const [search,setSearch] = useState('')
+
+const addItem = (item) => {
+  const id =  items.length ? items[items.length - 1].id + 1 : 1;
+  const addNewItem = {id,checked:false,item}
+  const listItems = [...items,addNewItem]
+  setItems(listItems)
+  localStorage.setItem('todo_list',JSON.stringify(listItems))
+
+}
+
+ const handleCheck = (id) => {
+   const listItems = items.map((item) => 
+   item.id === id ? {...item,checked:!item.checked} : item)
+   setItems(listItems)
+   localStorage.setItem('todo_list',JSON.stringify(listItems))
+ }
+
+ const handleDelete = (id) => {
+    const listItems = items.filter((item) => 
+   item.id !==id )
+  setItems(listItems)
+  localStorage.setItem('todo_list',JSON.stringify(listItems))
+
+    
+ }
+
+ const handleSubmit = (e) => {
+       e.preventDefault()
+       console.log('submitted')
+       if(!newItem) return;
+   console.log(newItem)
+
+   addItem(newItem)
+   setNewItem ('')
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title ='Add Something' />
+      < AddItem
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+      />
+      <SearchItem 
+      search = {search}
+      setSearch = {setSearch}
+      />
+      <Content
+        items = {items.filter(item => (item.item).toLowerCase().includes(search.toLowerCase()))}
+        handleCheck = {handleCheck}
+        handleDelete = {handleDelete}
+      />
+      <Footer 
+      length = {items.length}
+      />
+      
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ItemsList from './ItemsList';
+const Content = ({items,setItems,handleCheck,handleDelete}) => {
+  
+  return (
+    <main >
+        {(items.length) ? (
+            < ItemsList
+            items = {items}
+            handleCheck = {handleCheck}
+            handleDelete = {handleDelete}
+            />
+       
+        ) : (
+            <p style={ { marginTop: '2rem'}}>Your List is Empty!</p>
+        )} 
+    </main>
+  )
+}
+
+export default Content
